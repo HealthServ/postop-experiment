8c5f87dd53456c75e0010089f8f1c818031e3a1b
Hecker <julianheckerdev@gmail.com>
2022-10-06 00:51:50 -0400

    try something else in webview

--- pose_mobile/components/Pose/Pose.tsx
+++ pose_mobile/components/Pose/Pose.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useRef } from 'react';
-import { StyleSheet } from 'react-native';
+import { Button, StyleSheet } from 'react-native';
 import WebView from 'react-native-webview';
 import { defaultPoseProps, PoseProps } from './types';
 
@@ -7,20 +7,29 @@ export const Pose: FC<PoseProps> = (props = defaultPoseProps) => {
   const webRef = useRef<WebView>(null!);
 
   return (
-    <WebView
-      style={styles.container}
-      ref={webRef}
-      source={{
-        // webcam will not work without https
-        uri: 'https://healthserv.github.io/postop-experiment/',
-      }}
-      allowsInlineMediaPlayback
-      startInLoadingState
-      javaScriptCanOpenWindowsAutomatically={true}
-      mediaPlaybackRequiresUserAction={false}
-      allowsFullscreenVideo={false}
-      mediaCapturePermissionGrantType="grantIfSameHostElsePrompt"
-    />
+    <>
+      <WebView
+        style={styles.container}
+        ref={webRef}
+        source={{
+          // webcam will not work without https
+          uri: 'https://healthserv.github.io/postop-experiment/',
+        }}
+        allowsInlineMediaPlayback
+        startInLoadingState
+        javaScriptCanOpenWindowsAutomatically={true}
+        mediaPlaybackRequiresUserAction={false}
+        allowsFullscreenVideo={false}
+        mediaCapturePermissionGrantType="grantIfSameHostElsePrompt"
+        onMessage={(message) => console.log(JSON.stringify(message.nativeEvent.data))}
+      />
+      <Button
+        title="SEND DATA"
+        onPress={() => {
+          webRef.current.postMessage('THIS IS SOME DATA');
+        }}
+      />
+    </>
   );
 };
 
